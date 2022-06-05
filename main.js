e76c6e62068ec532eba949a99612f1671e81503f
murrayn808 <murrayn@hawaii.edu>
2022-06-04 19:17:59 -1000

    fixed issues with broken image and id names

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
     console.log("ready!");
 
-    $("#button").hover(function(){
+    $("#button_toggle").hover(function(){
         $(this).css("background-color", "violet");
     },function(){
         $(this).css("background-color", "orange");
@@ -10,33 +10,9 @@ $(document).ready(function() {
 
 
     $("button").html( "Click for a surprise!" )
-    $("button").on("click", function( event ) {
-        $("p").toggleClass('chungus compound');
-
-        let fboi = document.getElementById("fboi");
-        fboi.src = "https://cdn3.emoji.gg/emojis/9378-fuckboi.png";
-      
-        
-
     
-        /*if(){
-
-        }
-        */
-
-        /*
-        $("#fboi").show('noshow');        
-        $("#fboi").toggleClass('noshow');
-        */
-
-        /*if($("#fboi").attr("noshow") == true){
-            $("#fboi").attr("show", "noshow");
-        }
-
-        else{
-            $("#fboi").attr("noshow", "show");
-        }
-        */
+    $("#button_toggle").click(function () {
+        $("#fboi").toggle();
     });
 
 });
